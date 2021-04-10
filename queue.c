e7b4c61b86a6b8488b7758084135a8aca9d8ae34
ChungHsuanChen <chunghsuanchen7@gmail.com>
2021-04-10 15:37:34 +0800

    Complete In April
    "Complete q_insert_head() and q_insert_tail() function"

--- queue.c
+++ queue.c
@@ -47,13 +47,31 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (!q)
+        return NULL;
     list_ele_t *newh;
     /* TODO: What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
+    if (!newh)
+        return false;
+    newh->value = malloc(sizeof(char) * (strlen(s) + 1));
+
+    if (!newh->value) {
+        free(newh);
+        return false;
+    }
+
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
+
+    strncpy(newh->value, s, strlen(s));
+
     newh->next = q->head;
     q->head = newh;
+    if (!q->tail) {
+        q->tail = q->head;
+    }
+    q->size += 1;
     return true;
 }
 
@@ -66,10 +84,28 @@ bool q_insert_head(queue_t *q, char *s)
  */
 bool q_insert_tail(queue_t *q, char *s)
 {
-    /* TODO: You need to write the complete code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
-    return false;
+    if (!q)
+        return NULL;
+    list_ele_t *newh;
+    newh = malloc(sizeof(list_ele_t));
+    if (!newh)
+        return false;
+    newh->value = malloc(sizeof(char) * (strlen(s) + 1));
+    if (!newh->value) {
+        free(newh);
+        return false;
+    }
+    strncpy(newh->value, s, strlen(s));
+    newh->next = NULL;
+    if (!q->tail) {
+        q->head = newh;
+        q->tail = q->head;
+    } else {
+        q->tail->next = newh;
+        q->tail = q->tail->next;
+    }
+    q->size += 1;
+    return true;
 }
 
 /*
